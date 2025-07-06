f1e2284ded4b1fb78131e6782f75b90d21130e1a
RodrigueGeorges <rodrigue.georges@gmail.com>
2025-07-06 11:39:10 +0200

    MAJ37

--- src/pages/GuidanceAccess.tsx
+++ src/pages/GuidanceAccess.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { supabase } from '../lib/supabase';
-import { FormattedGuidanceText } from '../components/FormattedGuidanceText';
+import FormattedGuidanceText from '../components/FormattedGuidanceText';
 import LoadingScreen from '../components/LoadingScreen';
 
 export default function GuidanceAccess() {
